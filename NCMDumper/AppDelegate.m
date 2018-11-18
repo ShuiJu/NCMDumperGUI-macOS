073660e37c8a1836d978ebc573cfc5620332c872
Lakr Sakura <Lakr_Sakura@icloud.com>
2018-11-18 23:02:08 +0800

    init

---
+++ NCMDumper/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  NCMDumper
+//
+//  Created by Lakr Sakura on 2018/11/18.
+//  Copyright © 2018年 Lakr Sakura. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ NCMDumper/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  NCMDumper
+//
+//  Created by Lakr Sakura on 2018/11/18.
+//  Copyright © 2018年 Lakr Sakura. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
